908efb0c5ad556348271bd42d81117ec66133175
Ophir Bushinsky <ophirbushi@gmail.com>
2019-11-29 19:28:38 +0200

    remove non existing ref

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { GROUPED } from '../grouped';
 
 @Component({
   selector: 'app-admin',
@@ -9,22 +7,9 @@ import { GROUPED } from '../grouped';
 })
 export class AdminComponent implements OnInit {
 
-  constructor(private db: AngularFirestore) { }
 
   ngOnInit() {
-    console.log(GROUPED.center.length);
-    this.db.collection('center').valueChanges().subscribe(console.log);
 
-    GROUPED.center.forEach(person => {
-      this.addPerson(person);
-    });
-  }
-
-  addPerson(person) {
-    // Persist a document id
-    const id = this.db.createId();
-    person = { ...person, id, status: 0 };
-    this.db.collection('center').doc(id).set(person);
   }
 
 }
