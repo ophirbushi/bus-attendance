a952fb9935fab69a11458c81b252215a559aa50f
Ophir Bushinsky <ophirbushi@gmail.com>
2019-12-04 00:48:30 +0200

    admin-screen: manage bus groups wip

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,18 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormArray, FormControl, Validators } from '@angular/forms';
+import { BusGroupesState } from '../state/bus-groups.state';
+import { Subject } from 'rxjs';
+import { takeUntil, take } from 'rxjs/operators';
+import { BusGroup } from '../models';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss']
 })
-export class AdminComponent implements OnInit {
+export class AdminComponent implements OnInit, OnDestroy {
+  busGroupsForm: FormArray;
+  private readonly destroy = new Subject();
+
+  constructor(private busGroupsState: BusGroupesState) { }
 
   ngOnInit() {
+    this.busGroupsState.busGroups$
+      .pipe(
+        takeUntil(this.destroy),
+        take(1)
+      )
+      .subscribe(busGroups => {
+        this.busGroupsForm = new FormArray(
+          busGroups.map(group => this.createNewBusGroupFormGroup(group))
+        );
+      });
+  }
 
+  ngOnDestroy() {
+    this.destroy.next();
+    this.destroy.complete();
   }
 
   onFileInputChange(event: any) {
     console.log('onFileInputChange()', event);
+  }
+
+  addBusGroup() {
+    this.busGroupsForm.insert(this.busGroupsForm.length, this.createNewBusGroupFormGroup());
+  }
+
+  private createNewBusGroupFormGroup(existing?: BusGroup): FormGroup {
+    return new FormGroup({
+      _id: new FormControl(existing ? existing._id : null, Validators.required),
+      name: new FormControl(existing ? existing.name : null, Validators.required),
+      pickupPoints: new FormArray(
+        existing ? existing.pickupPoints.map(point => new FormGroup({
+          _id: new FormControl(point._id, Validators.required),
+          name: new FormControl(point.name, Validators.required)
+        })) : []
+      )
+    });
   }
 
 }
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,14 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin.component';
 import { MatButtonModule } from '@angular/material/button';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [AdminComponent],
   imports: [
     CommonModule,
     MatButtonModule,
-    FormsModule
+    MatInputModule,
+    MatCardModule,
+    MatTabsModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class AdminModule { }
